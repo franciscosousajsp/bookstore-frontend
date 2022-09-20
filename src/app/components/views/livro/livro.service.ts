7b5dc14d49e1bff2213d6bc042efbf79e199288f
franciscosousajsp <franciscosousajsp@gmail.com>
2022-09-20 15:28:00 -0300

    criacao do read livros

---
+++ src/app/components/views/livro/livro.service.ts
@@ -0,0 +1,23 @@
+import { ObserversModule } from '@angular/cdk/observers';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Livro } from './livro.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivroService {
+
+  baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  findAllByCategoria(id_cat: string): Observable<Livro[]>{
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`
+    return this.http.get<Livro[]>(url)
+  }
+
+}
